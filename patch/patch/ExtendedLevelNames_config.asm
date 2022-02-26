2c729679390882f527c7e522cb16033ac253a020
Naomatosis <naomatosis@gmail.com>
2022-02-27 00:59:38 +0100

    Inserted Level 22

--- patch/patch/ExtendedLevelNames_config.asm
+++ patch/patch/ExtendedLevelNames_config.asm
@@ -116,8 +116,8 @@ NameData:
  db "how eerie, an eerie"
  db "                   "
  ;Level 022
- db "Good Level Design  "
- db "REALLY?            "
+ db "   THE OBSTACLE    "
+ db "                   "
  ;Level 023
  db "  The Feeling of   "
  db "    a Freshman     "
