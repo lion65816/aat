5f249fc67ab97728672ec21ce56baad89e063ca6
lion65816 <whyhellothereimalion@gmail.com>
2022-07-03 03:08:17 +0100

    new level 1B updated.

---
+++ uberasm/level/1B.asm
@@ -0,0 +1,33 @@
+init:
+	LDA $141D|!addr
+	ORA $13CF|!addr
+	BEQ +		; intro
+	STZ $1692|!addr
++
+	RTL
+
+main:
+    LDX #!sprite_slots-1
+-
+    LDA !14C8,x
+    CMP #$08
+    BCC +
+    LDA !9E,x
+    CMP #$1E
+    BNE +
+    LDA !167A,x
+    ORA #$80
+    STA !167A,x
+    LDA !166E,x
+    ORA #$30
+    STA !166E,x
+    LDA !1686,x
+    ORA #$08
+    STA !1686,x
+    JSL $01A7DC|!bank
+    BCC +
+    JSL $00F5B7|!bank
++
+    DEX
+    BPL -
+    RTL
