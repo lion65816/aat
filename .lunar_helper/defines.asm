978e26d7e0bf1dd8094696d928947c0249cbe02f
lion65816 <whyhellothereimalion@gmail.com>
2023-01-26 13:49:02 +0000

    updated to lm3.33, lunar helper updated to 4.0.0

---
+++ .lunar_helper/defines.asm
@@ -0,0 +1,19 @@
+includeonce
+
+; Asar compatible file containing information about Lunar Helper, feel free to inscsrc this if needed,
+; it is recreated before every (Quick) Build
+
+; Define containing Lunar Helper's version number as a string
+!LH_VERSION = "4.0.0"
+
+; Defines containing Lunar Helper's version number as individual numbers
+; For example, if assembled with Lunar Helper 2.6.0, MAJOR will be 2, MINOR will be 6 and PATCH will be 0
+!LH_VERSION_MAJOR = 4
+!LH_VERSION_MINOR = 0
+!LH_VERSION_PATCH = 0
+
+; Define that just serves as a marker so you can check if a file is being assembled using Lunar Helper if needed
+!LH_ASSEMBLING = 1
+
+; Define that contains the path to Lunar Helper's globule folder, useful for calling globules without having to hardcode a user-specific path to the globule
+!LH_GLOBULES_FOLDER = C:/Users/David/Documents/GitHub/a3mt/.lunar_helper/globules/
