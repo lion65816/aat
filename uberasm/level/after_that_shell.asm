6594bb2e998a431c90394112e23ec134c06ee1bc
PSI Ninja <psininja@protonmail.com>
2024-05-13 22:15:41 -0500

    On/Off Switch resets to "On" when retrying Mall room 140

--- uberasm/level/after_that_shell.asm
+++ uberasm/level/after_that_shell.asm
@@ -1,16 +1,20 @@
 !screen_num = $0D
 
+load:
+	STZ $14AF|!addr				; Reset switch status to "On".
+	RTL
+
 init:
 	JSL freescrollbabey_init
 	JSL RequestRetry_init
 	RTL
 
 main:
-    LDA ($19B8+!screen_num)|!addr
-    STA $0C
-    LDA ($19D8+!screen_num)|!addr
-    STA $0D
-    JSL MultipersonReset_main
+	LDA ($19B8+!screen_num)|!addr
+	STA $0C
+	LDA ($19D8+!screen_num)|!addr
+	STA $0D
+	JSL MultipersonReset_main
 
 	LDA #%00110000 : STA $00	;\ Retry if pressing L+R.
 	JSL RequestRetry_main		;/
