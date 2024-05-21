8c7a49d95e9de1cbc37d10c3b653a037a93e3daa
PSI Ninja <psininja@protonmail.com>
2024-05-20 22:12:18 -0500

    Added No Spinjump sign to An Infinite Dream intro

---
+++ uberasm/level/LevelIntro135.asm
@@ -0,0 +1,44 @@
+init:
+	JSL DisableSideExit_init
+	JSR draw_sign_prep
+	JSL DrawSign_init			;> Input parameters are $00-$07.
+	RTL
+
+main:
+	JSL freezetimer_main
+	JSR draw_sign_prep
+	JSL DrawSign_main			;> Input parameters are $00-$07.
+	RTL
+
+draw_sign_prep:
+	;> Draw filter signs for mushroom, fire, and cape.
+	LDA #$11					;\ Request 17 tiles
+	STA $00						;| to draw the signs.
+	STZ $01						;/
+	LDA.b #TileCoord			;\ Store 24-bit address
+	STA $02						;| to the tile coordinate data.
+	LDA.b #TileCoord>>8			;|
+	STA $03						;|
+	LDA.b #TileCoord>>16		;|
+	STA $04						;/
+	LDA.b #TileProps			;\ Store 24-bit address
+	STA $05						;| to the tile property data.
+	LDA.b #TileProps>>8			;|
+	STA $06						;|
+	LDA.b #TileProps>>16		;|
+	STA $07						;/
+	RTS
+
+TileCoord:						; YYXX
+	dw $B088,$A088				; Sign post
+	dw $9080,$9090,$8080,$8090	; Sign face (bottom) (tile order: down -> up, left -> right)
+	dw $7080,$7090,$6080,$6090	; Sign face (middle) (tile order: down -> up, left -> right)
+	dw $5080,$5090,$4080,$4090	; Sign face (top) (tile order: down -> up, left -> right)
+	dw $8888,$6888,$4888		; Sign icons (bottom to top: cape, fire, "A")
+
+TileProps:						; High byte = YXPPCCCT, low byte = tile number
+	dw $29C9,$29C9				; Sign post
+	dw $A9A7,$69C7,$A9C7,$69A7	; Sign face (bottom) (tile order: down -> up, left -> right)
+	dw $A9A7,$69C7,$A9C7,$69A7	; Sign face (middle) (tile order: down -> up, left -> right)
+	dw $A9A7,$69C7,$A9C7,$69A7	; Sign face (top) (tile order: down -> up, left -> right)
+	dw $240E,$2A26,$2280		; Sign icons (bottom to top: cape, fire, "A")
