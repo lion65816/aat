4a9a8797d84a4dda93df3f7e5a2be1d5d8b39279
PSI Ninja <psininja@protonmail.com>
2024-06-11 23:43:04 -0400

    Update to credits UberASM
    - Can now speed up/slow down credits by holding right or left (courtesy of Daizo).

--- uberasm/level/Credits.asm
+++ uberasm/level/Credits.asm
@@ -1,7 +1,13 @@
 ;================================;
 ;quickass autowalk by mathos edited by Tattletale
+;but also edited by Daizo, which is a thing apparently
 ;================================;
-!Speed = $2A    ; player x speed: see https://www.smwcentral.net/?p=nmap&m=smwram#7E007B for details
+
+; limit should be between $00-$7F, otherwise you'll walk backwards
+!SlowSpeed = $00
+!MedSpeed = $10    ; player x speed: see https://www.smwcentral.net/?p=nmap&m=smwram#7E007B for details
+!FastSpeed = $40
+
 !Direction = 1  ; 1 = right, 0 = left : see https://www.smwcentral.net/?p=nmap&m=smwram#7E0076 for details
                 ; make sure it's coherent with your speed !
 
@@ -25,6 +31,7 @@
 ;Free RAM addresses to help control the walking animation/pose for Demo.
 !FreeRAM1 = $1696|!addr
 !FreeRAM2 = $18F6|!addr
+!FreeRAM3 = $18C5|!addr
 
 ;===========================================================================================================================================
 
@@ -37,29 +44,57 @@ load:
 	RTL
 
 init:
+	LDA $7E : STA $18C6|!addr
     LDA.b #!Direction
 	STA $76
+
+	LDA.b #!MedSpeed
+	STA !FreeRAM3
+
 	STZ !FreeRAM1
 	STZ !FreeRAM2
 	RTL
 
 main:
+	; Walk speed system
+	LDA $15 : AND #$03 ; left or right
+	TAY
+	LDX #$01
+-	LDA !FreeRAM3
+	CMP .speeds,y
+	BEQ +
+	BCS .bigger
+	INC !FreeRAM3
+	BRA ++
+.bigger
+	DEC !FreeRAM3
+++	DEX : BPL -
+	BRA +
+	.speeds
+	db !MedSpeed,!FastSpeed,!SlowSpeed,!FastSpeed
++
+
+	; print cart system
+	JSR PrintCart
+
 	LDA #$08
 	STA $1496|!addr
-	INC !FreeRAM1
-	LDA !FreeRAM1
-	CMP #$08
-	BNE +
-	STZ !FreeRAM1
-	INC !FreeRAM2
-	LDA !FreeRAM2
-	CMP #$03
-	BNE +
-	STZ !FreeRAM2
-
-	+
-	LDA !FreeRAM2
-	STA $13E0|!addr
+;	INC !FreeRAM1
+;	LDA !FreeRAM1
+;	CMP #$08
+;	BNE +
+;	STZ !FreeRAM1
+;	INC !FreeRAM2
+;	LDA !FreeRAM2
+;	CMP #$03
+;	BNE +
+;	STZ !FreeRAM2
+;
+;	+
+;	LDA !FreeRAM2
+	LDA $7B : BEQ +
+	LDA $94 : LSR #2 : AND #$03 : CMP #$03 : BNE + : DEC #2 : +
++	STA $13E0|!addr
 	LDA $71
 	CMP #$06
 	BNE +
@@ -87,7 +122,7 @@ main:
     LDA $77
     AND.b #(((!Direction+1)%2)+1)
     BNE .nospd
-    LDA.b #!Speed
+    LDA !FreeRAM3
 	STA $7B
     .nospd:
     
@@ -97,4 +132,45 @@ main:
 	TRB $18
 	RTL
 	RTL
-    
+   
+
+	; old fasioned way; idk how maxtile works.
+PrintCart:
+
+	REP #$20
+;	LDA $D1 : SEC : SBC $1A
+	LDA $D1 : SEC : SBC $1462|!addr ; least jarring 1 frame delay (I can't figure out how to fix it)
+;	LDA $94 : SEC : SBC $1A
+;	LDA $94 : SEC : SBC $1462|!addr
+	STA $00
+	SEP #$20
+
+;	LDA $94
+;	LSR #2 : AND #$01
+;	ASL : STA $00
+
+;	LDA $010B
+;	SEC : SBC #$E1
+;	ASL ;#2 ; 4
+	;CLC : ADC $00
+;	STA $00
+
+	LDY #$B0
+	LDX #$01
+-
+	LDA $00 : CLC : ADC .xpos,x : STA $0200|!addr,y
+	LDA $80 : CLC : ADC #$10 : STA $0201|!addr,y
+	
+	LDA .carttile,x : STA $0202|!addr,y
+	LDA #$63 : STA $0203|!addr,y
+	
+	PHY : TYA : LSR #2 : TAY
+	LDA #$02 : STA $0420|!addr,y
+	PLY : INY #4
+	DEX : BPL -
+
+	RTS
+.xpos
+	db $20-6,$10-6
+.carttile
+	db $A0,$A2
